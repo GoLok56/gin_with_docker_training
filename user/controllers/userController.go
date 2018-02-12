aada4bc7625b89da6e260f8bdfa172663c720383
Satria Adi Putra <satadii11@gmail.com>
2018-02-12 22:35:54 +0700

    add register controller and modify login to check to database

--- user/controllers/userController.go
+++ user/controllers/userController.go
@@ -5,24 +5,61 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/spf13/cast"
+
+	"../config"
+	"../models"
 )
 
 // Login Check for user's username and password with a registered user in database
 func Login(data ...interface{}) (int, map[string]interface{}) {
 	mapData := cast.ToStringMap(data[0])
+	db := config.OpenConnection()
+	defer db.Close()
 
-	if mapData["username"] == "golok" && mapData["password"] == "123456" {
-		return http.StatusOK, gin.H{
-			"error":   "false",
-			"message": "Welcome",
-			"status":  http.StatusOK,
-			"data":    mapData,
+	user := models.User{}
+
+	db.Where("username = ?", mapData["username"]).First(&user)
+	if user == (models.User{}) {
+		return http.StatusUnauthorized, gin.H{
+			"error":   "true",
+			"status":  http.StatusUnauthorized,
+			"message": "User not found!",
 		}
 	}
 
-	return http.StatusUnauthorized, gin.H{
-		"error":   true,
-		"message": "Check username/password",
-		"status":  http.StatusUnauthorized,
+	if user.Password != mapData["password"] {
+		return http.StatusUnauthorized, gin.H{
+			"error":   "true",
+			"status":  http.StatusUnauthorized,
+			"message": "Password doesn't match!",
+		}
+	}
+
+	return http.StatusOK, gin.H{
+		"error":   "false",
+		"status":  http.StatusOK,
+		"message": "Login success, welcome " + user.Name,
+	}
+}
+
+// Register Register a new user to database
+func Register(data ...interface{}) (int, map[string]interface{}) {
+	mapData := cast.ToStringMap(data[0])
+	db := config.OpenConnection()
+	defer db.Close()
+
+	user := models.User{
+		Username: mapData["username"].(string),
+		Password: mapData["password"].(string),
+		Name:     mapData["name"].(string),
+	}
+
+	db.Create(&user)
+
+	return http.StatusOK, gin.H{
+		"error":   false,
+		"status":  http.StatusOK,
+		"message": "Success",
+		"data":    user,
 	}
 }
